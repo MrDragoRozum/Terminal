4b69f06387843b87bee97a1ac735a33d68310e98
Alexander Rozum <roozumaleks@gmail.com>
2024-06-23 19:13:23 +0300

    Remember terminal state

--- app/src/main/java/ru/rozum/terminal/presentation/MainActivity.kt
+++ app/src/main/java/ru/rozum/terminal/presentation/MainActivity.kt
@@ -21,16 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TerminalTheme {
-//                val viewModel: TerminalViewModel = viewModel()
-//                val screenState = viewModel.state.collectAsState()
-//
-//                when(val currentState = screenState.value) {
-//                    is TerminalScreenState.Content -> {
-//                        Terminal(bars = currentState.barList)
-//                    }
-//                    TerminalScreenState.Initial -> {}
-//                }
-                Test()
+                val viewModel: TerminalViewModel = viewModel()
+                val screenState = viewModel.state.collectAsState()
+
+                when(val currentState = screenState.value) {
+                    is TerminalScreenState.Content -> {
+                        Terminal(bars = currentState.barList)
+                    }
+                    TerminalScreenState.Initial -> {}
+                }
             }
         }
     }
--- app/src/main/java/ru/rozum/terminal/presentation/Terminal.kt
+++ app/src/main/java/ru/rozum/terminal/presentation/Terminal.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.layout.onSizeChanged
 import ru.rozum.terminal.data.Bar
 import kotlin.math.roundToInt
 
@@ -24,43 +21,22 @@ private const val MIN_VISIBLE_BARS_COUNT = 20
 @Composable
 fun Terminal(bars: List<Bar>) {
 
-    var visibleBarsCount by remember {
-        mutableIntStateOf(100)
-    }
-
-    var scrolledBy by remember {
-        mutableFloatStateOf(0f)
-    }
-
-    var terminalWidth by remember {
-        mutableFloatStateOf(0f)
-    }
-
-    val barWidth by remember {
-        derivedStateOf {
-            terminalWidth / visibleBarsCount
-        }
-    }
-
-    val visibleBars by remember {
-        derivedStateOf {
-            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(0)
-            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(bars.size)
-            bars.subList(startIndex, endIndex)
-        }
-    }
+    var terminalState by rememberTerminalState(bars = bars)
 
     val transformableState = TransformableState { zoomChange, panChange, _ ->
 
-        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt().coerceIn(
+        val visibleBarsCount = (terminalState.visibleBarsCount / zoomChange).roundToInt().coerceIn(
             MIN_VISIBLE_BARS_COUNT, bars.size
         )
 
-
-        scrolledBy = (scrolledBy + panChange.x).coerceIn(
-            0f, bars.size * barWidth - terminalWidth
+        val scrolledBy = (terminalState.scrolledBy + panChange.x).coerceIn(
+            0f, bars.size * terminalState.barWidth - terminalState.terminalWidth
         )
 
+        terminalState = terminalState.copy(
+            visibleBarsCount = visibleBarsCount,
+            scrolledBy = scrolledBy
+        )
     }
 
     Canvas(
@@ -68,14 +44,16 @@ fun Terminal(bars: List<Bar>) {
             .fillMaxSize()
             .background(Color.Black)
             .transformable(transformableState)
+            .onSizeChanged {
+                terminalState = terminalState.copy(terminalWidth = it.width.toFloat())
+            }
     ) {
-        terminalWidth = size.width
-        val maxPrice = visibleBars.maxOf { it.high }
-        val minPrice = visibleBars.minOf { it.low }
+        val maxPrice = terminalState.visibleBars.maxOf { it.high }
+        val minPrice = terminalState.visibleBars.minOf { it.low }
         val pxPerPoint = size.height / (maxPrice - minPrice)
-        translate(left = scrolledBy) {
+        translate(left = terminalState.scrolledBy) {
             bars.forEachIndexed { index, bar ->
-                val offsetX = size.width - (barWidth * index)
+                val offsetX = size.width - (terminalState.barWidth * index)
                 drawLine(
                     color = Color.White,
                     start = Offset(offsetX, size.height - ((bar.low - minPrice) * pxPerPoint)),
@@ -86,7 +64,7 @@ fun Terminal(bars: List<Bar>) {
                     color = if (bar.open < bar.close) Color.Green else Color.Red,
                     start = Offset(offsetX, size.height - ((bar.open - minPrice) * pxPerPoint)),
                     end = Offset(offsetX, size.height - ((bar.close - minPrice) * pxPerPoint)),
-                    strokeWidth = barWidth / 2
+                    strokeWidth = terminalState.barWidth / 2
                 )
             }
         }
